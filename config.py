e30a90435268a8e990d353a2657024dd7ab1a46a
niganze <niganzealain@gmail.com>
2025-01-30 10:58:13 +0200

    deployment on render feature & erorr

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:1@localhost/netpipo_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://netpipo_db_user:CnMxWQmq7v2TVLaOW4c8SaBi8wubvCfD@dpg-cudio83tq21c738fes60-a.oregon-postgres.render.com/netpipo_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "supersecretkey"
 
