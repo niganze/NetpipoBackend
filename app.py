ecee0fe5acc95993fd7f84ce330a82923740c352
niganze <niganzealain@gmail.com>
2025-01-30 18:53:09 +0200

    feat: Configure Render deployment settings

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ from models.db import db
 from flask_migrate import Migrate
 from routes.employee_routes import employee_ns
 import os
+
 app = Flask(__name__)
 
 # Initialize Swagger
 api = Api(app, version='1.0', title='Employee API', description='A simple Employee API', doc='/swagger')
 
-# Use a test configuration for testing
+# Load the configuration from the environment
 app.config.from_object(Config)
 
 # Initialize the database with Flask and configure it with Flask-Migrate
@@ -21,6 +22,6 @@ migrate = Migrate(app, db)
 api.add_namespace(employee_ns, path='/api/employees')
 
 if __name__ == '__main__':
-    # Use dynamic port binding for Render deployment, defaulting to port 5000 locally
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    # Bind to the port defined by the PORT environment variable, with a fallback to 5000
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
